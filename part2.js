066e7e4ddf1d1a7ea8c0e9472c2926180269ea01
Ruth <ruthlangill@gmail.com>
2022-02-02 15:15:22 -0800

    added h2 p2 js for playing with numbers

---
+++ part2.js
@@ -0,0 +1,28 @@
+function calculate(){
+  let num1 = document.getElementById('number1').value;
+  let num2 = document.getElementById('number2').value;
+  let num3 = document.getElementById('number3').value;
+
+  n1 = parseInt(num1);
+  n2 = parseInt(num2);
+  n3 = parseInt(num3);
+
+  sum = n1 + n2 + n3
+  avg = sum/3
+  product = n1*n2*n3
+  min = Math.min(n1,n2,n3)
+  max = Math.max(n1,n2,n3)
+
+  if (isNaN(sum)){
+    window.alert('You must enter all 3 numbers to calulate')
+    return
+  }
+  results = "The sum is: "+sum +"\n The average is: " + avg + "\n The product is: " + product +"\n The smallest number is: " + min +"\n The largest number is: "+ max
+  
+  let display = document.getElementById('results').innerHTML = results
+  return display
+}
+
+function fadeResults(){
+  return
+}
