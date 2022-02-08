7f2543920e9a6b49acf2ac1166d0a86c19932445
Ruth <ruthlangill@gmail.com>
2022-02-08 15:20:22 -0800

    fixed variables

--- part2.js
+++ part2.js
@@ -6,21 +6,23 @@ function calculate(){
   let num2 = document.getElementById('number2').value;
   let num3 = document.getElementById('number3').value;
 
-  n1 = parseInt(num1);
-  n2 = parseInt(num2);
-  n3 = parseInt(num3);
+  let n1 = parseInt(num1);
+  let n2 = parseInt(num2);
+  let n3 = parseInt(num3);
 
-  sum = n1 + n2 + n3;
-  avg = sum/3;
-  product = n1*n2*n3;
-  min = Math.min(n1,n2,n3);
-  max = Math.max(n1,n2,n3);
+  let sum = n1 + n2 + n3;
+  let avg = sum/3;
+  let product = n1*n2*n3;
+  let min = Math.min(n1,n2,n3);
+  let max = Math.max(n1,n2,n3);
+  
+  let results = " The sum is: "+sum +"\n The average is: " + avg + "\n The product is: " + product +"\n The smallest number is: " + min +"\n The largest number is: "+ max;
 
   if (isNaN(sum)){
-    document.getElementById('error').innerHTML = 'You must enter all 3 whole numbers to calulate';
+    document.getElementById('error').innerText = 'You must enter all 3 whole numbers to calulate';
     return; 
   } else {
-  results = " The sum is: "+sum +"\n The average is: " + avg + "\n The product is: " + product +"\n The smallest number is: " + min +"\n The largest number is: "+ max;
+  
   document.getElementById('results').value = results;
 }
 }
