f304279fe30593ec13ca989587458e45fe4658ac
Ruth <ruthlangill@gmail.com>
2022-02-15 20:24:57 -0800

    fixed logic for negative number

--- hw3-js/hw3-1.js
+++ hw3-js/hw3-1.js
@@ -18,41 +18,42 @@ function calculate (){
   finalAverage = finalAverage.toFixed(0)
 
   if (isNaN(finalAverage)){
-    console.log('nan')
+    //console.log('nan')
     message.innerText = 'The input you put was invalid please try again with 4 numbers'
   } 
   else if (finalAverage > 100){
-    console.log('to big')
+    //console.log('to big')
     message.innerText = 'The numbers you entered were to large please try again with number between 0 and 100'
+    return
   } 
-  else if (finalAverage<0){
-    message.innerText = 'Please try again with whole numbers'
-    console.log('toosmall')
+  else if (hw <= -1 || mid <=0 || fin <= -1 || par <=0){
+    message.innerText = 'At least one of the numbers you entered was negative please try again with positive numbers'
+    return
   } 
   else if (finalAverage > 0 && finalAverage < 60){
-    console.log('f')
+    //console.log('f')
     message.innerText = "Student must retake the course"
     score.value = finalAverage
     letter.value = 'F'
   } 
   else if (finalAverage > 59 && finalAverage < 70){
-    console.log('d')
+    //console.log('d')
     message.innerText = "Student must retake the course"
     score.value = finalAverage
     letter.value = 'D'
   } 
   else if (finalAverage > 69 && finalAverage < 80){
-    console.log('c')
+    //console.log('c')
     score.value = finalAverage
     letter.value = 'C'
   } 
   else if (finalAverage > 79 && finalAverage < 90){
-    console.log('b')
+    //console.log('b')
     score.value = finalAverage
     letter.value = 'B'
   } 
   else if(finalAverage > 89) {
-    console.log('a')
+    //console.log('a')
     score.value = finalAverage
     letter.value = 'A'
   }
