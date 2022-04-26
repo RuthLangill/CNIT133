ce4283855a529c97841a9939bfd8d09f1856cc6f
Ruth <ruthlangill@gmail.com>
2022-04-26 07:52:50 -0400

    homework assignments 5 and 6

--- hw4-js/hw4-extra.js
+++ hw4-js/hw4-extra.js
@@ -1,7 +1,7 @@
 function box(){
   let height = document.getElementById('height').value 
   let width = document.getElementById('width').value 
-  
+  let rows
   for(let x = width; x>0; x--){
     document.write(' * ')
     for(let y=height; y>0; y--){
